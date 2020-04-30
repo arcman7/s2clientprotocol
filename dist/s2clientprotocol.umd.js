4488e9538357c3be94550e104e37b40c5559e8b4
Andrew Carpenter <andrewryancarpenter77@gmail.com>
2020-04-29 18:19:11 -0700

    fix imports

--- dist/s2clientprotocol.umd.js
+++ dist/s2clientprotocol.umd.js
@@ -4,16 +4,16 @@
     (global = global || self, factory(global.s2clientprotocol = {}));
 }(this, (function (exports) { 'use strict';
 
-    var common_pb = require('../s2clientprotocol/common_pb.js');
-    var data_pb = require('../s2clientprotocol/data_pb.js');
-    var debug_pb = require('../s2clientprotocol/debug_pb.js');
-    var error_pb = require('../s2clientprotocol/error_pb.js');
-    var query_pb = require('../s2clientprotocol/query_pb.js');
-    var raw_pb = require('../s2clientprotocol/raw_pb.js');
-    var sc2api_pb = require('../s2clientprotocol/sc2api_pb.js');
-    var score_pb = require('../s2clientprotocol/score_pb.js');
-    var spatial_pb = require('../s2clientprotocol/spatial_pb.js');
-    var ui_pb = require('../s2clientprotocol/ui_pb.js');
+    var common_pb = require('./common_pb.js');
+    var data_pb = require('./data_pb.js');
+    var debug_pb = require('./debug_pb.js');
+    var error_pb = require('./error_pb.js');
+    var query_pb = require('./query_pb.js');
+    var raw_pb = require('./raw_pb.js');
+    var sc2api_pb = require('./sc2api_pb.js');
+    var score_pb = require('./score_pb.js');
+    var spatial_pb = require('./spatial_pb.js');
+    var ui_pb = require('./ui_pb.js');
 
     var sc2clientprotocol = {
         common_pb,
